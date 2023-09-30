01397737a4852c1970db616287144d65f20f3c21
mahanfarzaneh2000 <mahan.farzaneh@gmail.com>
2023-09-30 20:24:30 +0330

    first example and a simple command-line arguments

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,11 @@
+use std::{fs, env::args};
+
 mod elf;
 mod instruct_table;
 
 fn main() {
+    let mut args = args();
+    args.next().unwrap();
+    let source = fs::read_to_string(args.next().unwrap()).expect("can not Read the file");
+    println!("{}",source.len());
 }
